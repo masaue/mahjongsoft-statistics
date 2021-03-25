a1ef4c56caf0c0d0453bc5e51772105807028b17
masaue <masayuki.uegaki@gmail.com>
2021-03-26 07:55:14 +0900

    login and show my sessions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import readlineSync from 'readline-sync';
+
 import Scraper from './scraper';
 
 (async () => {
+  const login = readlineSync.question('login?');
+  const password = readlineSync.questionNewPassword('password?');
+
   const scraper = new Scraper();
   await scraper.initialize();
-  await scraper.statistics();
+  await scraper.statistics(login, password);
 })();
--- src/scraper.ts
+++ src/scraper.ts
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 export default class Scraper {
   private readonly TARGET = 'https://mahjongsoft.com/sessions.php';
   private readonly SCRIPT = 'https://mahjongsoft.com/_sessions.php';
+  private readonly LOGIN = 'https://mahjongsoft.com/_login.php';
   private page!: puppeteer.Page;
 
   constructor () {}
@@ -14,8 +15,30 @@ export default class Scraper {
     await this.gotoTarget();
   }
 
-  async statistics() {
-    await this.showAllSessions();
+  async statistics(login: string, password: string) {
+    await this.login(login, password);
+    await this.showMySessions();
+  }
+
+  private async clickLoginButton() {
+    await Promise.all([
+      this.page.waitForResponse(this.LOGIN),
+      this.page.click('#login_button'),
+    ]);
+    // for hiding login modal
+    await Promise.all([
+      //this.page.waitForFunction('document.getElementById("navbar_text").value !== "Offline"'),
+      //this.page.waitForSelector('#loginModal', {visible: false}),
+      //this.page.waitForSelector('#logout_button', {visible: true}),
+      // TODO use another waitforXXXX()
+      this.page.waitForTimeout(1000),
+      this.page.click('#sessionsShowButton'),
+    ]);
+  }
+
+  private async fillLoginAndPassword(login: string, password: string) {
+    await this.page.type('#loginLoginInput', login);
+    await this.page.type('#loginPasswordInput', password);
   }
 
   private async gotoTarget() {
@@ -25,11 +48,25 @@ export default class Scraper {
     ]);
   }
 
-  private async showAllSessions() {
+  private async login(login: string, password: string) {
+    await this.showLoginModal();
+    await this.fillLoginAndPassword(login, password);
+    await this.clickLoginButton();
+  }
+
+  private async showMySessions() {
     await this.page.click('#sessionsLastWeekCheck');
+    await this.page.click('#sessionsWithMeCheck');
     await Promise.all([
       this.page.waitForResponse(this.SCRIPT),
       this.page.click('#sessionsShowButton'),
+    ]);
+  }
+
+  private async showLoginModal() {
+    await Promise.all([
+      this.page.waitForSelector('#loginModal', {visible: true}),
+      this.page.click('#login_form_button'),
     ]);
   }
 }
